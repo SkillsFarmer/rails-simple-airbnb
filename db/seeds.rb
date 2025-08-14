12232aff0da87794a10025a596013c522ec444ff
Rayan Walidi-Alaoui <wldrayan21@gmail.com>
2025-08-14 08:42:49 +0200

    Exercise rails simple-Airbnb (unfinished)

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# 1. Clean the database 🗑️
+puts 'Cleaning database...'
+Flat.destroy_all
+
+# 2. Create the instances 🏗️
+puts 'Creating flats...'
+Flat.create!(name: 'Flat 1', address: '7 Boundary St, London E2 7JE', description: 'Some description',
+             price_per_night: 150, number_of_guests: 3)
+puts 'Flat 1'
+Flat.create!(name: 'Flat 2', address: '56A Shoreditch High St, London E1 6PQ', description: 'Some description',
+             price_per_night: 150, number_of_guests: 3)
+puts 'Flat 2'
+Flat.create!(name: 'Flat 3', address: '7 Boundary St, London E2 7JE', description: 'Some description',
+             price_per_night: 150, number_of_guests: 3)
+puts 'Flat 3'
+Flat.create!(name: 'Flat 4', address: '56A Shoreditch High St, London E1 6PQ', description: 'Some description',
+             price_per_night: 150, number_of_guests: 3)
+puts 'Flat 4'
+
+# 3. Display a message 🎉
+puts "Finished! Created #{Flat.count} flats."
